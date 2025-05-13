29d19a987e200ccde9e745bda839c79b04e2f499
techigit <srinugithub88@gmail.com>
2025-05-13 16:13:14 +0530

    WEQRQR

--- src/main/java/com/org/java/controller/EmployeeController.java
+++ src/main/java/com/org/java/controller/EmployeeController.java
@@ -37,7 +37,7 @@ public class EmployeeController {
 	public String welcomeTest() {
 		logger.trace("it is trace method welcome calling");
 		logger.info("wel come method is working");
-		return "WELCOME TO AWS TEST CI/CD DONE BY SRINU";
+		return "WELCOME TO AWS TEST CI/CD TESTING BY USING AWS CLOULD..";
 	}
 
 	@PostMapping("/save")
